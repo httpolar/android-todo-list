0bf3b8f2ac1f5dec7c8f7c4e82781453c8bdd9a6
httpolar <96192210+httpolar@users.noreply.github.com>
2023-06-16 16:46:12 +0400

    Switched to kotlin scripts and list now shows as cards

--- app/src/main/kotlin/com/github/httpolar/todolist/MainActivity.kt
+++ app/src/main/kotlin/com/github/httpolar/todolist/MainActivity.kt
@@ -9,17 +9,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -27,12 +30,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import com.github.httpolar.todolist.ui.theme.TodoListTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,9 +46,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class TaskData(
+    val content: String = "",
+    val id: UUID = UUID.randomUUID(),
+)
+
 data class TodoListState(
     val textInput: String = "",
-    val tasks: List<String> = listOf(),
+    val search: String = "",
+    val tasks: List<TaskData> = listOf(),
 )
 
 class TodoListViewModel : ViewModel() {
@@ -54,9 +63,13 @@ class TodoListViewModel : ViewModel() {
 
     fun setTextInput(s: String) {
         _state.update { current ->
-            current.copy(
-                textInput = s
-            )
+            current.copy(textInput = s)
+        }
+    }
+
+    fun setSearch(s: String) {
+        _state.update { current ->
+            current.copy(search = s)
         }
     }
 
@@ -64,11 +77,17 @@ class TodoListViewModel : ViewModel() {
         _state.update { current ->
             current.copy(
                 tasks = buildList {
-                    add(current.textInput)
+                    add(TaskData(current.textInput))
                     addAll(current.tasks)
                 },
                 textInput = "",
             )
+        }
+    }
+
+    fun removeTask(id: UUID) {
+        _state.update { current ->
+            current.copy(tasks = current.tasks.filter { it.id != id })
         }
     }
 }
@@ -81,16 +100,33 @@ fun TodoListScreen() {
             color = MaterialTheme.colorScheme.background,
             modifier = Modifier.fillMaxSize(),
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                UserInput()
-                Tasks()
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    UserInput()
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(16.dp)
+                    )
+                    Tasks()
+                }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TodoListAppBar(viewModel: TodoListViewModel = viewModel()) {
+    val state by viewModel.state.collectAsState()
+
+    TopAppBar(
+        title = { Text(text = "Todo") },
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -117,22 +153,36 @@ fun UserInput(viewModel: TodoListViewModel = viewModel()) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Task(data: TaskData, viewModel: TodoListViewModel = viewModel()) {
+    Card(
+        onClick = { viewModel.removeTask(data.id) },
+        shape = RoundedCornerShape(4.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(text = data.content)
+        }
+    }
+}
+
 @Composable
 fun Tasks(viewModel: TodoListViewModel = viewModel()) {
     val state by viewModel.state.collectAsState()
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 4.dp)
-    ) {
-        itemsIndexed(state.tasks) { _, item ->
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        itemsIndexed(state.tasks) { _, task ->
             Column(modifier = Modifier.fillMaxWidth()) {
-                Text(text = item, fontSize = 16.sp)
-                Divider(
+                Task(task)
+                Spacer(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(bottom = 2.dp, top = 2.dp)
+                        .height(4.dp)
                 )
             }
         }
